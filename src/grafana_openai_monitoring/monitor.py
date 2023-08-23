5e2a562190cc8ac1b327de207edafe421f2cf0de
ishanjainn <ishan.jain0099@gmail.com>
2023-08-23 15:38:14 -0400

    fix linting

--- src/grafana_openai_monitoring/monitor.py
+++ src/grafana_openai_monitoring/monitor.py
@@ -6,11 +6,11 @@ def __check(metrics_url, logs_url, metrics_username, logs_username, access_token
     # Check if all required parameters are provided
     if not all([metrics_url, logs_url, metrics_username, logs_username, access_token]):
         raise ValueError("All parameters (metrics_url, logs_url, metrics_username, logs_username, access_token) must be provided")
-    
+
     # Check if 'api/prom' is present in the metrics URL
     if "api/prom" not in metrics_url:
         raise ValueError("Invalid metrics URL format. It should contain 'api/prom' in the URL.")
-    
+  
     # Convert metrics_url to use the influx line protocol url
     if "prometheus" in metrics_url:
         metrics_url = metrics_url.replace("prometheus", "influx")
@@ -19,7 +19,7 @@ def __check(metrics_url, logs_url, metrics_username, logs_username, access_token
         # Special case exception for prometheus-us-central1
         if "-us-central1" in metrics_url:
             metrics_url = metrics_url.replace("-us-central1", "-prod-06-prod-us-central-0")
-    
+
     # Return metrics_url and logs_url without the trailing slash
     return metrics_url[:-1] if metrics_url.endswith('/') else metrics_url, logs_url[:-1] if logs_url.endswith('/') else logs_url
 
@@ -37,12 +37,12 @@ def __calculate_cost(model, prompt_tokens, sampled_tokens):
         "gpt-4": (0.03, 0.06),
         "gpt-gpt-4-32k": (0.06, 0.12),
     }
-    
+
     prompt_price, sampled_price = prices.get(model, (0, 0))
-    
+
     # Calculate the total cost based on prompt and sampled tokens
     cost = (prompt_tokens / 1000) * prompt_price + (sampled_tokens / 1000) * sampled_price
-    
+
     return cost
 
 # Function to send logs to the specified logs URL
@@ -51,8 +51,8 @@ def __send_logs(logs_url, logs_username, access_token, logs):
         response = requests.post(logs_url, auth=(logs_username, access_token), json=logs, headers={"Content-Type": "application/json"})
         response.raise_for_status()  # Raise an exception for non-2xx status codes
         return response
-    except requests.exceptions.RequestException as e:
-        raise requests.exceptions.RequestException(f"Error sending Logs: {e}")
+    except requests.exceptions.RequestException as err:
+        raise requests.exceptions.RequestException(f"Error sending Logs: {err}")
 
 # Function to send metrics to the specified metrics URL
 def __send_metrics(metrics_url, metrics_username, access_token, metrics):
@@ -61,13 +61,13 @@ def __send_metrics(metrics_url, metrics_username, access_token, metrics):
         response = requests.post(metrics_url, headers={'Content-Type': 'text/plain'}, data=str(body), auth=(metrics_username, access_token))
         response.raise_for_status()  # Raise an exception for non-2xx status codes
         return response
-    except requests.exceptions.RequestException as e:
-        raise requests.exceptions.RequestException(f"Error sending Metrics: {e}") 
+    except requests.exceptions.RequestException as err:
+        raise requests.exceptions.RequestException(f"Error sending Metrics: {err}") 
 
 # Decorator function to monitor chat completion
-def chatV2(func, metrics_url, logs_url, metrics_username, logs_username, access_token):
+def chat_v2(func, metrics_url, logs_url, metrics_username, logs_username, access_token):
     metrics_url, logs_url = __check(metrics_url, logs_url, metrics_username, logs_username, access_token)
-    
+
     def wrapper(*args, **kwargs):
         start_time = time.time()
         response = func(*args, **kwargs)
@@ -77,10 +77,10 @@ def chatV2(func, metrics_url, logs_url, metrics_username, logs_username, access_
         # Determine prompt and model from args or kwargs
         prompt = args[1] if args and len(args) > 1 and isinstance(args[1], str) else kwargs.get('messages', [{"content": "No prompt provided"}])[0]['content']
         model = args[2] if len(args) > 2 and isinstance(args[2], str) else kwargs.get('model', "No model provided")
-        
+
         # Calculate the cost based on the response's usage
         cost = __calculate_cost(model, response.usage.prompt_tokens, response.usage.completion_tokens)
-        
+
         # Prepare logs to be sent
         logs = {
             "streams": [
@@ -99,10 +99,10 @@ def chatV2(func, metrics_url, logs_url, metrics_username, logs_username, access_
                 }
             ]
         }
-        
+
         # Send logs to the specified logs URL
-        loki_response = __send_logs(logs_url=logs_url, logs_username=logs_username, access_token=access_token, logs=logs)
-        
+        __send_logs(logs_url=logs_url, logs_username=logs_username, access_token=access_token, logs=logs)
+
         # Prepare metrics to be sent
         metrics = [
             # Metric to track the number of completion tokens used in the response
@@ -120,10 +120,10 @@ def chatV2(func, metrics_url, logs_url, metrics_username, logs_username, access_
             # Metric to track the duration of the API request and response cycle
             f'openai,integration=openai,source=python,model={response.model} requestDuration={duration}',
         ]
-        
+
         # Send metrics to the specified metrics URL
-        prometheus_response = __send_metrics(metrics_url=metrics_url, metrics_username=metrics_username, access_token=access_token, metrics=metrics)
-        
+        __send_metrics(metrics_url=metrics_url, metrics_username=metrics_username, access_token=access_token, metrics=metrics)
+
         return response
 
     return wrapper
